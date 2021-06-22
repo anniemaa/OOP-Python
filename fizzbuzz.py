07b38f80fe8b90553cf8ef50368879163841be85
anniemaa <46156148+anniemaa@users.noreply.github.com>
2021-06-22 03:18:45 -0700

    Initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzBuzz(self, n: int) -> List[str]:
+    output_list = []
+    for i in range(1, n + 1):
+        output = ""
+        if i % 3 == 0:
+            output += 'Fizz'
+        if i % 5 == 0:
+            output += 'Buzz'
+        if output == "":
+            output += str(i)
+        output_list.append(output)
+    return output_list
